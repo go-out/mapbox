49023a6bd15b71d4a034c62e6084733372c6c899
Kazuma Sasajima <sorryforthedelayinsending@vg.pe.hu>
2024-01-06 23:54:24 +0900

    Create mikunigaoka.js

---
+++ marker/mikunigaoka.js
@@ -0,0 +1,292 @@
+'use strict'
+
+const mikunigaoka = {
+    'type': 'FeatureCollection',
+    'features': [
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.4835, 34.5738]
+            },
+            'properties': {
+                'title': '堺市役所前',
+                'address': '〒590-0078 大阪府堺市堺区南瓦町３',
+                'date': '',
+                'iconSize': ['https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Ionicons_bicycle-outline.svg/240px-Ionicons_bicycle-outline.svg.png', '3rem', '3rem'],
+                'tags': '',
+                'zoom': 15,
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.48419, 34.56022]
+            },
+            'properties': {
+                'title': '大仙公園いこいの広場',
+                'address': '〒590-0802 大阪府堺市堺区百舌鳥夕雲町２−１８',
+                'date': '',
+                'iconSize': ['https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Ionicons_bicycle-outline.svg/240px-Ionicons_bicycle-outline.svg.png', '3rem', '3rem'],
+                'tags': '',
+                'zoom': 15,
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.50075, 34.57103]
+            },
+            'properties': {
+                'title': 'サンヴァリエ金岡',
+                'address': '〒591-8046 大阪府堺市北区東三国ヶ丘町５丁８',
+                'date': '',
+                'iconSize': ['https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Ionicons_bicycle-outline.svg/240px-Ionicons_bicycle-outline.svg.png', '3rem', '3rem'],
+                'tags': '',
+                'zoom': 15,
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.49214, 34.56537]
+            },
+            'properties': {
+                'title': '三国ヶ丘駅前西第１自転車駐車場',
+                'address': '〒590-0026 大阪府堺市堺区向陵西町４丁１１',
+                'date': '',
+                'iconSize': ['https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Ionicons_bicycle-outline.svg/240px-Ionicons_bicycle-outline.svg.png', '3rem', '3rem'],
+                'tags': '',
+                'zoom': 15,
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.49667, 34.57854]
+            },
+            'properties': {
+                'title': 'サンスクエア堺',
+                'address': '〒590-0014 大阪府堺市堺区田出井町２',
+                'date': '',
+                'iconSize': ['https://upload.wikimedia.org/wikipedia/commons/thumb/8/85/Ionicons_bicycle-outline.svg/240px-Ionicons_bicycle-outline.svg.png', '3rem', '3rem'],
+                'tags': '',
+                'zoom': 15,
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.48299939522383,34.573390705714104]
+            },
+            'properties': {
+                'title': '堺市役所21階展望ロビー',
+                'address': '〒590-0078 大阪府堺市堺区南瓦町３−１',
+                'date': '',
+                'iconSize': ['https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Ionicons_alert-circle-outline.svg/240px-Ionicons_alert-circle-outline.svg.png', '3.21rem', '3.21rem'],
+                'tags': '',
+                'zoom': 15,
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.48141, 34.58119]
+            },
+            'properties': {
+                'title': '妙国寺',
+                'address': '〒590-0942 大阪府堺市堺区材木町東４丁１−４',
+                'date': '妙国寺庭園・大蘇鉄 が  文化遺産',
+                'iconSize': ['https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Ionicons_location-sharp.svg/240px-Ionicons_location-sharp.svg.png', '3rem', '3rem'],
+                'tags': '',
+                'zoom': 15,
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.48981, 34.57395]
+            },
+            'properties': {
+                'title': '旧天王貯水池',
+                'address': '〒590-0022 大阪府堺市堺区中三国ヶ丘町３丁３',
+                'date': '',
+                'iconSize': ['https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Ionicons_location-sharp.svg/240px-Ionicons_location-sharp.svg.png', '3rem', '3rem'],
+                'tags': '',
+                'zoom': 15,
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.48624, 34.56004]
+            },
+            'properties': {
+                'title': '百舌鳥古墳群ビジターセンター',
+                'address': '〒590-0802 大阪府堺市堺区百舌鳥夕雲町２丁１６０',
+                'date': '',
+                'iconSize': ['https://upload.wikimedia.org/wikipedia/commons/thumb/3/34/Ionicons_alert-circle-outline.svg/240px-Ionicons_alert-circle-outline.svg.png', '3.21rem', '3.21rem'],
+                'tags': '',
+                'zoom': 15,
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.48411, 34.55974]
+            },
+            'properties': {
+                'title': '堺市茶室 伸庵',
+                'address': '〒590-0802 大阪府堺市堺区百舌鳥夕雲町２丁',
+                'date': '',
+                'iconSize': ['https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Ionicons_location-sharp.svg/240px-Ionicons_location-sharp.svg.png', '3rem', '3rem'],
+                'tags': '',
+                'zoom': 15,
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.48426, 34.55955]
+            },
+            'properties': {
+                'title': '堺市茶室 黄梅庵',
+                'address': '〒590-0802 大阪府堺市堺区百舌鳥夕雲町２丁１９４',
+                'date': '',
+                'iconSize': ['https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Ionicons_location-sharp.svg/240px-Ionicons_location-sharp.svg.png', '3rem', '3rem'],
+                'tags': '',
+                'zoom': 15,
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.47895, 34.55623]
+            },
+            'properties': {
+                'title': '履中天皇陵展古墳展望所',
+                'address': '〒593-8301 大阪府堺市西区上野芝町１丁１７−４',
+                'date': '',
+                'iconSize': ['https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Ionicons_location-sharp.svg/240px-Ionicons_location-sharp.svg.png', '3rem', '3rem'],
+                'tags': '',
+                'zoom': 15,
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.49725, 34.57854]
+            },
+            'properties': {
+                'title': '東雲公園',
+                'address': '〒590-0013 大阪府堺市堺区東雲西町１丁８',
+                'date': '',
+                'iconSize': ['https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Ionicons_location-sharp.svg/240px-Ionicons_location-sharp.svg.png', '3rem', '3rem'],
+                'tags': '',
+                'zoom': 15,
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.49819, 34.5701]
+            },
+            'properties': {
+                'title': '金岡団地スターハウスメモリアル',
+                'address': '〒591-8046 大阪府堺市北区東三国ヶ丘町２丁１−２３',
+                'date': '',
+                'iconSize': ['https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Ionicons_home-outline.svg/240px-Ionicons_home-outline.svg.png', '2rem', '2rem'],
+                'tags': '',
+                'zoom': 15,
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.50069, 34.55584]
+            },
+            'properties': {
+                'title': '筒井家住宅',
+                'address': '〒591-8023 大阪府堺市北区中百舌鳥町４丁５３５',
+                'date': '',
+                'iconSize': ['https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Ionicons_home-outline.svg/240px-Ionicons_home-outline.svg.png', '2rem', '2rem'],
+                'tags': '',
+                'zoom': 15,
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.49715, 34.55871]
+            },
+            'properties': {
+                'title': '旧是枝近有邸',
+                'address': '〒591-8031 大阪府堺市北区百舌鳥梅北町４丁１８５',
+                'date': '',
+                'iconSize': ['https://upload.wikimedia.org/wikipedia/commons/thumb/7/73/Ionicons_home-outline.svg/240px-Ionicons_home-outline.svg.png', '2rem', '2rem'],
+                'tags': '',
+                'zoom': 15,
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.50108, 34.5711]
+            },
+            'properties': {
+                'title': 'フォーユー',
+                'address': '〒591-8046 大阪府堺市北区東三国ヶ丘町５丁８−７',
+                'date': '',
+                'iconSize': ['../profile/img/1010.png', '2rem', '2rem'],
+                'tags': '',
+                'zoom': 15,
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [ 135.49108, 34.56999]
+            },
+            'properties': {
+                'title': '世界パン',
+                'address': '〒590-0026 大阪府堺市堺区向陵西町１丁９−１３',
+                'date': '',
+                'iconSize': ['https://upload.wikimedia.org/wikipedia/commons/thumb/0/0f/Ionicons_restaurant-outline.svg/240px-Ionicons_restaurant-outline.svg.png', '1.75rem', '1.75rem'],
+                'tags': '',
+                'zoom': 15,
+            }
+        },
+        {
+            'type': 'Feature',
+            'geometry': {
+                'type': 'Point',
+                'coordinates': [135.49783, 34.5662]
+            },
+            'properties': {
+                'title': 'ジェラッテリア&カフェ リカリカ',
+                'address': '〒590-0025 大阪府堺市堺区向陵東町１丁２−２０',
+                'date': '',
+                'iconSize': ['https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Ionicons_ice-cream-outline.svg/240px-Ionicons_ice-cream-outline.svg.png', '2rem', '2rem'],
+                'tags': '',
+                'zoom': 15,
+            }
+        }
+    ]
+};
